3e09aea8e2c160be8ce7634f00a97cdebe7d66d8
anri775 <jimsheleishvili775@gmail.com>
2024-09-27 16:40:14 +0400

    first commit

---
+++ homework1/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className = "about">
+      <h1>anri</h1>
+      <h1>jimsheleishvili</h1>
+    </div>
+  )
+}
+
+export default App
